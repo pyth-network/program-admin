31d7e94ebaf1b0e9554fd6b5a275613847d245bf
Thomaz Leite <thomaz@pyth.network>
2022-04-25 08:25:57 -0300

    Show publisher names in list-accounts

--- program_admin/cli.py
+++ program_admin/cli.py
@@ -1,10 +1,12 @@
 import asyncio
 import sys
+from pathlib import Path
 
 import click
 from solana.publickey import PublicKey
 
 from program_admin import ProgramAdmin
+from program_admin.parsing import parse_publishers_json
 
 
 @click.group()
@@ -15,7 +17,10 @@ def cli():
 @click.command()
 @click.option("--network", help="Solana network", envvar="NETWORK")
 @click.option("--program-key", help="Pyth program key", envvar="PROGRAM_KEY")
-def list_accounts(network, program_key):
+@click.option(
+    "--publishers", help="Path to reference publishers file", envvar="PUBLISHERS"
+)
+def list_accounts(network, program_key, publishers):
     program_admin = ProgramAdmin(
         network=network,
         program_key=program_key,
@@ -29,19 +34,28 @@ def list_accounts(network, program_key):
         print("Program has no mapping accounts")
         sys.exit(1)
 
+    publishers_map = parse_publishers_json(Path(publishers))
+
     while mapping_key != PublicKey(0):
         mapping_account = program_admin.get_mapping_account(mapping_key)
-        print(f"Mapping: {mapping_account}")
+        print(f"Mapping: {mapping_account.public_key}")
 
         for product_key in mapping_account.data.product_account_keys:
             product_account = program_admin.get_product_account(product_key)
-            print(f"  Product: {product_account}")
+            print(f"  Product: {product_account.data.metadata['symbol']}")
 
             if product_account.data.first_price_account_key != PublicKey(0):
                 price_account = program_admin.get_price_account(
                     product_account.data.first_price_account_key
                 )
-                print(f"    Price: {price_account}")
+                print(
+                    f"    Price: {price_account.data.exponent} exponent ({price_account.data.components_count} components)"
+                )
+
+                for component in price_account.data.price_components:
+                    print(
+                        f"      Publisher: {publishers_map['names'][component.publisher_key]}"
+                    )
 
         mapping_key = mapping_account.data.next_mapping_account_key
 
